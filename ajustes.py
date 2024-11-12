8d57ac0340ce93dd1e27ace3f331b159a7668b12
Eliu-C <eliu.corvalan@mail.udp.cl>
2024-11-12 05:20:02 -0300

    Update ajustes.py

--- ajustes.py
+++ ajustes.py
@@ -1,10 +1,15 @@
 from Crypto.Random import get_random_bytes
 
-def ajustar_iv(iv):
-    tamano_iv = 8
+def ajustar_iv(iv, algoritmo):
+    if algoritmo == 'AES-256':
+        tamano_iv = 16
+    else:
+        tamano_iv = 8
+
     if len(iv) < tamano_iv:
         iv += get_random_bytes(tamano_iv - len(iv))
     elif len(iv) > tamano_iv:
         iv = iv[:tamano_iv]
-    print(f"IV ajustado:", iv)
+
+    print(f"IV ajustado: {iv}")
     return iv
